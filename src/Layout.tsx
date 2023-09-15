44b0ba084d150ba3c0919eca10db8d18318d2d43
grafek <jacek.grafender@gmail.com>
2023-09-15 22:18:32 +0200

    spacing

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,14 +1,19 @@
 import { Toaster } from "react-hot-toast";
 
-type Props = { children: React.ReactNode };
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
 
-export default function Layout({ children }: Props) {
+export default function Layout({ children, className }: Props) {
   return (
     <>
       <header>
-        <h1 className="text-3xl text-center pt-2 pb-16">Music genre poll</h1>
+        <h1 className="text-3xl text-center pt-2 pb-12">Music genre poll</h1>
       </header>
-      <main className="container px-4 mx-auto h-[calc(100dvh-8rem)] flex flex-col items-center gap-10">
+      <main
+        className={`container px-4 mx-auto min-h-[calc(100dvh-8rem)] flex flex-col items-center gap-10 ${className}`}
+      >
         {children}
       </main>
       <Toaster />
