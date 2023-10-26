41f36a393f08960af055eb30e421a1271a94a29a
grafek <jacek.grafender@gmail.com>
2023-10-26 21:24:32 +0200

    loading skeleton adjustments, poll name

--- src/components/Poll.tsx
+++ src/components/Poll.tsx
@@ -5,7 +5,7 @@ type Props = { id: string; djSetDate: string; className?: string };
 export default function Poll({ id, djSetDate, className }: Props) {
   return (
     <a
-      href={`/polls/${id}`}
+      href={`/poll/${id}`}
       className={`px-3 flex-1 py-2 border border-primary-200 block rounded transition-colors duration-200 hover:bg-primary-400 ${className}`}
     >
       {dateFormatter(new Date(djSetDate))}
